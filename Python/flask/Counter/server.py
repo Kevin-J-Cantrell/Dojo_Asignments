91f4692594fdaca6460801bcd52d00018fa2af96
Kevin J Cantrell <112343044+Kevin-J-Cantrell@users.noreply.github.com>
2022-11-17 14:23:44 -0800

    assignment

--- Python/flask/Counter/server.py
+++ Python/flask/Counter/server.py
@@ -4,16 +4,28 @@ app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
 @app.route('/')
 def index():
-    if 'count' not in session:
+    if 'count'not in session:
+        session["count"]=1    
+    elif 'count' in session:
+        session["count"] = int(session['count']) + 2
+    else: 
         session["count"]=1
-    elif 'count' not in session:
-        session['count']+=request.form["count"]
-    else:
-        session["count"]+=2
     return render_template('index.html')
 
-@app.route('/clear')
-def clear():
+
+@app.route('/count', methods=["post"])
+def count():
+    print("FORM DATA",request.form)
+    print("count DATA",request.form)
+    if 'count' not in session:
+        count = session["count"]
+        count.append(request.form)
+        session["count"] = count
+    else:
+        session["count"] += int(request.form["count"]) -2
+    return redirect('/')
+@app.route('/destroy_session')
+def destroy_session():
     session.clear()
     return redirect('/')
 if __name__ == '__main__':
